ec8f2a07e7503df991eda0dddbb72a570d526287
Trevor Prosser <treedprosser@gmail.com>
2022-09-27 17:12:40 -0600

    add Google sign in

---
+++ components/Loader.js
@@ -0,0 +1,4 @@
+// Loading spinner
+export default function Loader({ show }) {
+    return show ? <div className="loader"></div> : null;
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,6 @@
+export default function AdminPostsPage({ }) {
+  return (
+    <main>
+    </main>
+  )
+}
