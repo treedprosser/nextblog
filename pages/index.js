0d6af93e3813240b5b7f0d296de5093fdcd73ce6
Trevor Prosser <treedprosser@gmail.com>
2022-09-28 19:43:52 -0600

    add user page and post feed

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,63 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import PostFeed from '../components/PostFeed';
+import Loader from '../components/Loader';
+import { firestore, fromMillis, postToJSON } from '../lib/firebase';
 
-import toast from 'react-hot-toast';
+import { useState } from 'react';
 
-export default function Home() {
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts =(await postsQuery.get()).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length -1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+
+    const query = firestore
+      .collectionGroup('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts (posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <button onClick={() => toast.success('Hello Toast!')}>Toast Me</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load More</button>}
+
+      <Loader show={loading} />
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
   );
 }
