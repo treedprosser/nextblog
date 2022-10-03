3b9a927d39624e964e95cb9d7a058f4a6ddd004e
Trevor Prosser <91508914+treedprosser@users.noreply.github.com>
2022-10-03 15:51:08 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home(props) {
       <Metatags title="Home Page" description="Get the latest posts on our site" />
 
       <div className="card card-info">
-        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <h2>💡 Next.js + Firebase</h2>
         <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
         <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
       </div>
